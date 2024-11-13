85466bea9fb1bb8b15683a140456795270aef69d
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-13 17:07:01 +0100

    fix: aggiunto if

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let totale = (chilometri * prezzomin);
 console.log(totale + ' ' + valuta);
 const maggioreta = 18;
 const over = 65;
+
+let scontoMin = (totale * 20) / 100
+let scontoOver= (totale * 40) / 100
+if(maggioreta < 18){
+  
+  console.log(scontoMin + ' ' + valuta);
+}     
+  
+// if(maggioreta > 65){
+//   console.log(ScontoOver); 
+// }  
+ 
+
+ 
